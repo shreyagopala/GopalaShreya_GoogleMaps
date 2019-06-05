5671ccbc5e02c5dea3b62336353dc7009596ee64
shreyagopala <shreya.gopala@gmail.com>
2019-06-05 13:42:31 -0700

    did stuff

--- MapsApp/app/src/main/java/com/example/mapsapp/MapsActivity.java
+++ MapsApp/app/src/main/java/com/example/mapsapp/MapsActivity.java
@@ -2,9 +2,13 @@ package com.example.mapsapp;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -17,6 +21,13 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LocationManager locationManager;
+
+    private boolean isGPSEnabled = false;
+    private boolean isNetworkEnabled = false;
+
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 5;
+    private static final float MIN_DISTANCE_CHANGE_FOR_UPDATE = 0.0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,10 +62,112 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-        public void changeView (View view){
-            if (mMap.getMapType() != mMap.MAP_TYPE_SATELLITE) {
-                mMap.setMapType(mMap.MAP_TYPE_SATELLITE);
-            } else
-                mMap.setMapType(mMap.MAP_TYPE_NORMAL);
+    public void changeView(View view) {
+        if (mMap.getMapType() != mMap.MAP_TYPE_SATELLITE) {
+            mMap.setMapType(mMap.MAP_TYPE_SATELLITE);
+        } else
+            mMap.setMapType(mMap.MAP_TYPE_NORMAL);
+    }
+
+    public void getLocation() {
+        try {
+            locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+            //get GPS status
+            isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            if (isGPSEnabled) Log.d("MyMapsApp", "getLocation: GPS is enabled");
+
+            //get Network status
+            isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            if (isNetworkEnabled) Log.d("MyMapsApp", "getLocation: Network is enabled");
+
+            //neither is enabled
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                Log.d("MyMapsApp", "getLocation: No provider is enabled");
+            } else {
+                if (isNetworkEnabled) {
+                    Log.d("MyMapsApp", "getLocation: network location update");
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATE, locationListenerNetwork);
+                }
+                if (isGPSEnabled) {
+                    Log.d("MyMapsApp", "getLocation: gps location update");
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATE, locationListenerGPS);
+                }
+            }
+        } catch (Exception e) {
+            Log.d("MyMapsApp", "Caught exception in getLocation");
+            e.printStackTrace();
         }
     }
+
+    LocationListener locationListenerNetwork = new LocationListener() {
+
+        @Override
+        public void onLocationChanged(Location location) {
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    LocationListener locationListenerGPS = new LocationListener() {
+
+
+        @Override
+        public void onLocationChanged(Location location) {
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
